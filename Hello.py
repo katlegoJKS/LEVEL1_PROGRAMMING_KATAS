d04421bec8619f7cc730fb65a1b63e646483ece5
katlegoJKS <katlego.malatjie@umuzi.org>
2020-02-03 15:51:32 +0200

    Exerecise 2

--- Hello.py
+++ Hello.py
@@ -1,3 +1,19 @@
+#Exercise: Hello
+print("------TASK 1------")
+
 def hello(name):
     print("Hello " + name)
 hello("Tshepo!")
+
+
+
+#Exercise: check if a number is even
+print("\n\n------TASK 2------")
+
+def even_or_odd(numb):
+    numb=int(input("Enter a number : "))
+    if(numb%2==0):
+        print("even")
+    else:
+        print("odd")
+even_or_odd("numb")
