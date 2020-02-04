658aac713c631a95f9e97dcbb9d4d57ed9c7c9eb
katlegoJKS <katlego.malatjie@umuzi.org>
2020-02-04 15:00:45 +0200

    Exercise 4

--- Level1ProgrammingKATAS.py
+++ Level1ProgrammingKATAS.py
@@ -44,3 +44,24 @@ def triangle(Numbr):
             print("#",end="")
         print()
 triangle(2)
+
+
+
+#Exercise: Draw an iscosceles triangle
+print("\n\n------Task 5------")
+
+def isosceles(num):
+    num = int(input("Enter: "))
+    row = 0
+    while row<num:
+        space = num-row-1
+        while space>0:
+            print(end=" ")
+            space = space-1
+        star = row+1
+        while star>0:
+            print("#",end=" ")
+            star = star-1
+        row = row+1
+        print()
+isosceles("")
