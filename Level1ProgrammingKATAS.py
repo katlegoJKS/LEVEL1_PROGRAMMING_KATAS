2b6268965fb717818f6932739f98bc2c3f53014a
katlegoJKS <katlego.malatjie@umuzi.org>
2020-02-04 11:51:30 +0200

    Exercise 3

--- Level1ProgrammingKATAS.py
+++ Level1ProgrammingKATAS.py
@@ -11,9 +11,23 @@ hello("Tshepo!")
 print("\n\n------TASK 2------")
 
 def even_or_odd(numb):
-    numb=int(input("Enter a number : "))
+    numb = int(input("Enter a number : "))
     if(numb%2==0):
         print("even")
     else:
         print("odd")
 even_or_odd("numb")
+
+
+
+#Exercise: Draw a square
+print("\n\n------Task 3------")
+
+def square(numb):
+    numb = int(input("Enter a number: "))
+    n_list = [["#" for x in range(numb)] for y in range(numb)]
+    for row in range(0,numb):
+        for col in range(0,numb):
+                print(n_list[row][col],end=" ")
+        print()
+square("#")
